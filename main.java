242c9480ed9699f53234054592b3729d74c5b761
bobowita <65597517+bobowita@users.noreply.github.com>
2021-12-02 16:44:50 +0000

    Add files via upload

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -7,7 +8,7 @@ public class main {
     public static void main(String[] args) {
 
 
-        ArrayList<String> hammers = new ArrayList<String>();
+        ArrayList<String> hammers = new ArrayList<>();
         hammers.add("ślusarski");
         hammers.add("kamieniarski");
         hammers.add("sledgehammer");
@@ -46,32 +47,23 @@ public class main {
 
         ArrayList<String> koszyk = new ArrayList<>();
 
-        int acc = 0;
         int exit = 0;
         Scanner sc1 = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
         Scanner sc3 = new Scanner(System.in);
         Scanner sc4 = new Scanner(System.in);
 
-        System.out.println("czy chcesz założyc konto 1 nie 2 tak : ");
-        int ii = sc2.nextInt();
+        System.out.println("czy chcesz założyc konto 1 nie 2 tak 3 zaloguj się: ");
 
-        if (ii >= 1 & ii <=2){
-            System.out.println("wszystko git");
-        } else {
-            System.out.println("zepsuleś");
-            System.exit(1);
+        try {
+            sklep.wyb();
+        } catch (InputMismatchException x){
+            System.out.println("Wprowadziłeś zły znak");
+            System.out.println("1.kontynułuj bez konta");
+            System.out.println("2.stwurz konto");
+            System.out.println("3.zaloguj się");
+            sklep.wyb();
         }
 
-        switch (ii) {
-            case 1:
-                System.out.println("lecisz bez konta");
-                break;
-            case 2:
-                acc = 1;
-                person.logowanie();
-                break;
-        }
 
         while (exit == 0) {
 
@@ -81,61 +73,60 @@ public class main {
             }
             int categg = sc1.nextInt();
 
-            switch (categg) {
-                case 1:
-                    for (int i = 0; i < hammers.size(); i++) {
-                        System.out.println(hammers.get(i));
-                    }
-                    break;
-                case 2:
-                    for (int i = 0; i < glue.size(); i++) {
-                        System.out.println(glue.get(i));
-                    }
-                    break;
-                case 3:
-                    for (int i = 0; i < ziptie.size(); i++) {
-                        System.out.println(ziptie.get(i));
-                    }
-                    break;
-                case 4:
-                    for (int i = 0; i < tape.size(); i++) {
-                        System.out.println(tape.get(i));
-                    }
-                    break;
+        switch (categg) {
+            case 1:
+                for (int i = 0; i < hammers.size(); i++) {
+                System.out.println(hammers.get(i));
+                }
+                break;
+            case 2:
+                for (int i = 0; i < glue.size(); i++) {
+                System.out.println(glue.get(i));
+                }
+                break;
+            case 3:
+                for (int i = 0; i < ziptie.size(); i++) {
+                System.out.println(ziptie.get(i));
+                }
+                break;
+            case 4:
+                for (int i = 0; i < tape.size(); i++) {
+                System.out.println(tape.get(i));
+                }
+                break;
                 case 5:
-                    exit = 1;
-                    break;
+                exit = 1;
+                break;
             }
 
-            switch (categg) {
-                case 1:
-                    System.out.println("podaj numer produktu : ");
-                    int pr1 = sc3.nextInt();
-                    System.out.println("dodałeś do koszyka : " + hammers.get(pr1 - 1));
-                    koszyk.add(hammers.get(pr1 - 1));
-                    break;
-                case 2:
-                    System.out.println("podaj numer produktu : ");
-                    int pr2 = sc3.nextInt();
-                    System.out.println("dodałeś do koszyka : " + glue.get(pr2 - 1));
-                    koszyk.add(glue.get(pr2 - 1));
-                    break;
-                case 3:
-                    System.out.println("podaj numer produktu : ");
-                    int pr3 = sc3.nextInt();
-                    System.out.println("dodałeś do koszyka : " + ziptie.get(pr3 - 1));
-                    koszyk.add(ziptie.get(pr3 - 1));
-                    break;
-                case 4:
-                    System.out.println("podaj numer produktu : ");
-                    int pr4 = sc3.nextInt();
-                    System.out.println("dodałeś do koszyka : " + tape.get(pr4 - 1));
-                    koszyk.add(tape.get(pr4 - 1));
-                    break;
+        switch (categg) {
+            case 1:
+                System.out.println("podaj numer produktu : ");
+                int pr1 = sc3.nextInt();
+                System.out.println("dodałeś do koszyka : " + hammers.get(pr1 - 1));
+                koszyk.add(hammers.get(pr1 - 1));
+                break;
+            case 2:
+                System.out.println("podaj numer produktu : ");
+                int pr2 = sc3.nextInt();
+                System.out.println("dodałeś do koszyka : " + glue.get(pr2 - 1));
+                koszyk.add(glue.get(pr2 - 1));
+                break;
+            case 3:
+                System.out.println("podaj numer produktu : ");
+                int pr3 = sc3.nextInt();
+                System.out.println("dodałeś do koszyka : " + ziptie.get(pr3 - 1));
+                koszyk.add(ziptie.get(pr3 - 1));
+                break;
+            case 4:
+                System.out.println("podaj numer produktu : ");
+                int pr4 = sc3.nextInt();
+                System.out.println("dodałeś do koszyka : " + tape.get(pr4 - 1));
+                koszyk.add(tape.get(pr4 - 1));
+                break;
             }
         }
 
-        koszyk.get(0);
         System.out.println("twoje produkty: ");
 
         for (int i = 0; i < koszyk.size(); i++) {
@@ -143,18 +134,18 @@ public class main {
 
         System.out.println("czy chesz złożyć zamówienie: ");
         System.out.println("1 tak 2 nie");
-        int wybur = 0;
+        int wybur;
         wybur = sc4.nextInt();
 
         switch (wybur){
             case 1:
-                if (acc == 1){
+                if (sklep.acc == 1){
                     person.dane();
                 } else {
                     person.logowanie();
                 }
                 System.out.println("twoje założenie zostało złożone");
-                int randomNum = (int)(Math.random() * 1001);  // 0 to 1000
+                int randomNum = (int)(Math.random() * 101);  // 0 to 1000
                 System.out.println("twoje konto zostalo obciążone kwotą: " + randomNum);
 
                 break;
@@ -170,12 +161,8 @@ public class main {
 
 
     }
+    //voidy
 }
-/**
- * @autors Gruzdas Bartłomiej Dawid szemis
- * nie motoda switch ii pozwala wybrć czy zaklada sie konto
- * nie metoda switch categ pozwala wybrać kategorie produktu
- *  metoda pozwala założyc konto
- */
+
 
 
